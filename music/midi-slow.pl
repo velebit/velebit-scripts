2f3b1ea0786f7449c835dd4ceabf90c22c3370e0
bert <velebit@users.noreply.github.com>
2009-10-27 14:14:23 +0000

    A better command-line interface for midi-slow.pl

--- music/midi-slow.pl
+++ music/midi-slow.pl
@@ -1,31 +1,44 @@
 #!/usr/bin/perl
+# Change the speed/tempo of a MIDI file.
+# In other words, just like "midi-tweak --tempo" except it actually works.
+#
 use warnings;
 use strict;
 use MIDI;
+use Getopt::Long;
 
-sub slow_down( $$$ );
+### usage stuff
 
-for my $v ( qw( alto bass ) ) {
-  for my $p ( qw( 5_m851-end ) ) {
-    for my $spd (50, 67, 75) {
-      #print "... v=$v spd=$spd\n";
-
-      # midi-tweak doesn't work for cyberbass.com files.  Hate hate hate.
-#      system('/home/bert/perl-lib/bin/midi-tweak', '--tempo',
-#	     '--ratio', 100/$spd, '--ident',
-#	     '--output', "$v/${v}_${p}_spd0${spd}.mid", "$v/${v}_${p}.mid");
-
-      my $spdx = sprintf "%03d", $spd;
-      print "\n";
-      slow_down("$v/${v}_${p}.mid", 100/$spd, "$v/${v}_${p}_spd${spdx}.mid");
-    }
-  }
+sub Usage ( @ ) {
+  print STDERR join "\n\n", @_, <<EndOfUsage;
+Usage:
+  $0 [-s PERCENT[,PERCENT...]] infiles...
+or:
+  $0 -s PERCENT infile --to outfile
+EndOfUsage
+  exit 1;
 }
-print "\n";
+
+use vars qw( @SPEED @OUTPUT );
+GetOptions('speed|s=s'       => \@SPEED,
+	   'output|o|to|t=s' => \@OUTPUT,
+          ) or Usage;
+
+@ARGV
+  or  Usage "Error: you must specify at least one input file.";
+@OUTPUT and (@ARGV != 1 or @OUTPUT != 1)
+  and Usage "Error: you can't use --to with more than one file.";
+@OUTPUT and @SPEED != 1
+  and Usage "Error: you must specify -s exactly once when using --to.";
+
+@SPEED = map split(/,/), @SPEED;
+@SPEED = qw( 50 66.667 80 90 ) unless @SPEED;
+
+### code to perform the MIDI speed modification
 
 sub round ( $ ) { int($_[0] + .5); }
 
-sub slow_down ( $$$ ) {
+sub change_tempo ( $$$ ) {
   my ($in, $ratio, $out) = @_;
 
   my $opus = MIDI::Opus->new({from_file => $in}) or die;
@@ -43,3 +56,25 @@ sub slow_down ( $$$ ) {
   $opus->write_to_file($out);
   print "$out written.\n";
 }
+
+### do it!
+
+sub make_output_name ( $$ ) {
+  my ($file, $speed) = @_;
+  $file =~ s/\.midi?$//i;
+  $file =~ /_spd\d+$/
+    and die("I refuse to auto-generate a file name for an already modified file"
+	    . "\n    '$_[0]'\n ");
+  $file . sprintf("_spd%03d.mid", $speed);
+}
+
+for my $input (@ARGV) {
+  for my $speed (@SPEED) {
+    my $output = shift @OUTPUT;
+    $output = make_output_name $input, $speed unless defined $output;
+
+    print "\n";
+    change_tempo($input, 100/$speed, $output);
+  }
+}
+print "\n";
