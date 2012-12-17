3b9e93c8896c12894ca7ecc58d9fae2d20516675
bert <velebit@users.noreply.github.com>
2012-12-17 14:41:53 +0000

    NCFO: add M3U playlists too.

--- net-slurp/ncfo/playlists.sh
+++ net-slurp/ncfo/playlists.sh
@@ -17,7 +17,16 @@ generate_wpl () {
     echo "</smil>"
 }
 
-make_wpl () {
+generate_m3u () {
+    local name="$1"; shift
+
+    echo "## $name"
+    for i in "$@"; do
+	echo "$i"
+    done
+}
+
+make_playlist () {
     local dir="$1"
     local suffix="$2"
     local tracks="$3"
@@ -27,24 +36,28 @@ make_wpl () {
     set -- `ls "$dir"/*.[Mm][Pp]3 \
         | egrep "$tracks" | egrep -v "$ignore" | sort -t "$sep" -k 2n`
 
-    if [ "$#" -lt 1 ]; then rm -f "$dir$suffix.wpl"; return; fi
+    if [ "$#" -lt 1 ]; then rm -f "$dir$suffix".{wpl,m3u}; return; fi
     generate_wpl "$dir practice" "$@" > "$dir$suffix.wpl"
     unix2dos -q "$dir$suffix.wpl"
+    generate_m3u "$dir practice" "$@" > "$dir$suffix.m3u"
 }
 
 if [ "$#" -eq 0 ]; then set -- Abbe Katarina Meredith; fi
 for who in "$@"; do
-    rm -f "$who".wpl "$who"_*.wpl
-    #make_wpl "$who" '_all' . ''
-    #make_wpl "$who" '' \
+    rm -f "$who".{wpl,m3u} "$who"_*.{wpl,m3u}
+    #make_playlist "$who" '_all' . ''
+    #make_playlist "$who" '' \
     #  'Birth|Eras|LivingLight|Mutate|Reptiles|Axolotl|Cetac.ans|4E9|Hedgehog'\
     #  'Piano|Orch'
     #if diff -q "${who}.wpl" "${who}_all.wpl" > /dev/null; then
     #    rm -f "${who}_all.wpl"
     #fi
-    make_wpl "$who" '' . ''
-    rm -f "${who}_all.wpl"
-    #make_wpl "$who" _burn_tmp . 'Piano|Orch'
+    #if diff -q "${who}.m3u" "${who}_all.m3u" > /dev/null; then
+    #    rm -f "${who}_all.m3u"
+    #fi
+    make_playlist "$who" '' . ''
+    rm -f "${who}_all.wpl" "${who}_all.m3u"
+    #make_playlist "$who" _burn_tmp . 'Piano|Orch'
 done
 
 #./download/merge-playlists.pl -k \
