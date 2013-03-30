b4d436172d4373509726c2a0ab82094e495b9f8f
bert <velebit@users.noreply.github.com>
2013-03-30 13:08:41 +0000

    [NCFO] extras2process: read directives and only add matching extras.
    In addition to the list of extras directories on the command line, the
    script now also reads in a list of already prepared processing
    directives on stdin.  The output is all of the directives seen on
    stdin, *plus* all of the corresponding extras.  Extras for directories
    not seen on stdin will be ignored, with a warning message.
    
    This means this script must now be invoked as a "filter" for an
    existing process stream, not just added to it at the end.

--- net-slurp/ncfo/extras2process.pl
+++ net-slurp/ncfo/extras2process.pl
@@ -1,10 +1,45 @@
 #!/usr/bin/perl
+#
+# usage: extras2process [dirs...] [-- input-files...]
+#
+# Takes a list of extras directories on the command line, and a list
+# of already prepared processing directives on stdin.  The extras
+# directories must be named "*.DEST" or "DEST", where DEST is the
+# actual destination directory for the extras.
+#
+# The output is all of the directives seen on stdin, *plus* all of the
+# corresponding extras.  Extras for directories not seen on stdin will
+# be ignored, with a warning message.
+
 use warnings;
 use strict;
 
-for my $dir (@ARGV) {
+my %entries;
+while (@ARGV) {
+  my $dir = shift @ARGV;
+  $dir eq '--' and last;
   my @files = glob "$dir/*";
   s,.*/,, for @files;
   my $name = $dir;  $name =~ s,.*/,,;  $name =~ s,.*\.,,;
-  print "$dir/$_=../$name/$_\n" for @files;
+  push @{$entries{$name}}, map "$dir/$_=../$name/$_", @files;
 }
+
+my @entries = <>;
+s/\r?\n$// for @entries;
+
+sub out_path ( $ ) { (split /=/, $_[0], 2)[1]; }
+
+my @out_paths = map out_path($_), @entries;
+
+my @extra_entries;
+for my $name (sort keys %entries) {
+  if (scalar grep $_ =~ m!/\Q$name\E/!, @out_paths) {
+    push @extra_entries, @{$entries{$name}};
+  } else {
+    print STDERR "Skipped extra: $_\n"
+      for map out_path($_), @{$entries{$name}};
+  }
+}
+
+push @entries, @extra_entries;
+print "$_\n" for sort { out_path($a) cmp out_path($b) } @entries;
--- net-slurp/ncfo/opera/copy-files.sh
+++ net-slurp/ncfo/opera/copy-files.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./make-url-lists.sh
-(./urllist2process.pl *.mp3.urllist; ./extras2process.pl mp3-extras.*) \
-    | ./process-files.pl
+./urllist2process.pl *.mp3.urllist | ./extras2process.pl mp3-extras.* \
+    | ./process-files.pl $PF_ARGS
