e70afc29103e27144afa29895908afc67112261f
bert <velebit@users.noreply.github.com>
2013-03-30 13:08:43 +0000

    [NCFO] opera/copy-files: add --fast, like in he chorus variant.
    This is minor but useful for debugging and completeness.

--- net-slurp/ncfo/opera/copy-files.sh
+++ net-slurp/ncfo/opera/copy-files.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+
+PF_ARGS=
+while true; do
+    case "$1" in
+	-f|--fast)
+	    PF_ARGS='--no-gain --no-wipe'; shift ;;
+	-*)
+	    echo "Unknown flag '$1'!" >&2 ; exit 1 ;;
+	*)
+	    break ;;
+    esac
+done
+
 ./make-url-lists.sh
 ./urllist2process.pl *.mp3.urllist | ./extras2process.pl mp3-extras.* \
     | ./process-files.pl $PF_ARGS
