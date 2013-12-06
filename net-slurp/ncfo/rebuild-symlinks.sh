7ec9bbdc613ea5b5cfe509178edbcb06f8d3a4fb
bert <velebit@users.noreply.github.com>
2013-12-06 11:35:19 +0000

    [NCFO] Make rebuild-symlinks guess the type.
    Of course, it can only guess if there are some symlinks already...

--- net-slurp/ncfo/rebuild-symlinks.sh
+++ net-slurp/ncfo/rebuild-symlinks.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 
+if [ $# -lt 1 ]; then
+    # guess the subdirectory
+    set -- "`find * -type d -prune -o -type l -print0 \
+	    | xargs -0 -r -n 1 readlink \
+	    | sed -e 's,/[^/]*$,/,;s,.*/ncfo/,,;/^$/d;s,/$,,;/\//d' \
+            | uniq`"
+    if [ -z "$1" ]; then
+	echo "Could not guess link type..." >&2
+    else
+	echo "Guessed the type as '$1'..." >&2
+    fi
+fi
+
 case "$1" in
     opera)
 	subdir=opera ;;
     science|chorus)
 	subdir=science ;;
     *)
-	echo "Usage: `basename "$0"` {opera | science}" >&2
+	echo "If guessing is not possible, you must specify an argument!" >&2
+	echo "" >&2
+	echo "Usage: `basename "$0"` [opera | science]" >&2
 	exit 1 ;;
 esac
 
