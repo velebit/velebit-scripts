605e4266ff838da92c20b340e9152c0f077f20e7
bert <velebit@users.noreply.github.com>
2014-01-30 00:33:28 +0000

    [NCFO] Create video URL lists for opera.

--- net-slurp/ncfo/opera/make-url-lists.sh
+++ net-slurp/ncfo/opera/make-url-lists.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 INDEX="${1-mp3/index.html}"
+INDEX_PDF="${2-${INDEX}}"
+INDEX_VIDEO="${3-${INDEX}}"
+
 rm -f *.urllist
- ~/scripts/net-slurp/plinks.pl "$INDEX" \
-     | sed -e '/\.pdf$/!d;/Score/!d' > score.pdf.urllist
 
 ./extract-column-links.pl "$INDEX" \
     'Soprano Chorus MP3s' +0 'All' \
@@ -10,6 +11,9 @@ rm -f *.urllist
 ./extract-column-links.pl "$INDEX" \
     'Soprano Chorus MP3s' +1 'All' \
     | sed -e '/SopHi/d' >> Katarina.mp3.urllist
+~/scripts/net-slurp/plinks.pl -t "$INDEX_VIDEO" \
+    | sed -e '/\.mp4$/I!d;/^[^	]*chorus/I!d;s/^[^	]*	//' \
+    > Katarina.video.urllist
 
 ./extract-column-links.pl "$INDEX" \
     'Bass Chorus MP3s' +0 'All' \
@@ -17,6 +21,9 @@ rm -f *.urllist
 ./extract-column-links.pl "$INDEX" \
     'Bass Chorus MP3s' +1 'All' \
     | sed -e '/XXXnonesuchXXX/d' >> bert.mp3.urllist
+~/scripts/net-slurp/plinks.pl -t "$INDEX_VIDEO" \
+    | sed -e '/rain.*dance[^\/	]*\.mp4$/I!d;s/^[^	]*	//' \
+    > bert.video.urllist
 
 ./extract-column-links.pl "$INDEX" \
     'Alto Chorus MP3s' +0 'All' \
@@ -24,3 +31,6 @@ rm -f *.urllist
 ./extract-column-links.pl "$INDEX" \
     'Alto Chorus MP3s' +1 'All' \
     | sed -e '/XXXnonesuchXXX/d' >> alto.mp3.urllist
+
+~/scripts/net-slurp/plinks.pl "$INDEX_PDF" \
+     | sed -e '/\.pdf$/I!d;/Score/!d' > score.pdf.urllist
--- net-slurp/plinks.pl
+++ net-slurp/plinks.pl
@@ -3,19 +3,34 @@
 use warnings;
 use strict;
 
-use HTML::LinkExtor;
+use HTML::TreeBuilder;
 
 $| = 1;
 
+our $SHOW_TEXT = 0;
+while (@ARGV and $ARGV[0] =~ /^-/) {
+  $ARGV[0] eq '-t' and ++$SHOW_TEXT, shift(@ARGV), next;
+  die "Unknown argument '$ARGV[0]'";
+}
+@ARGV == 1 or die "Usage: $0 [-t] {FILE}\n";
+
+
+my $tree = HTML::TreeBuilder->new;
+$tree->parse_file($ARGV[0]);
+
 my @pages;
-my $build_links = HTML::LinkExtor->new(
-    sub {
-      my($tag, %attr) = @_;
-      return if lc $tag ne 'a';  # only <a ...> tags
-      push @pages, $attr{href} if exists $attr{href};
-    });
+# only look at <a ...> tags
+for my $tag ($tree->look_down(_tag => 'a')) {
+  my $href = $tag->attr('href')
+    or next;
+  my $text = $tag->as_text;
+  $text =~ s/\s+/ /sg;
+  $text =~ s/^ //;  $text =~ s/ $//;
+  push @pages, [$text, $href];
+}
 
-@ARGV == 1 or die "Usage: $0 {FILE}\n";
-$build_links->parse_file($ARGV[0]);
-
-print "$_\n" for @pages;
+if ($SHOW_TEXT) {
+  print "$_->[0]\t$_->[1]\n" for @pages;
+} else {
+  print "$_->[1]\n" for @pages;
+}
