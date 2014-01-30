b8c4e65ccdce4d41a48df49ca93f71166fe0cd3e
bert <velebit@users.noreply.github.com>
2014-01-30 00:33:37 +0000

    [NCFO] Update URL list generation with Rain Dance scene 7.

--- net-slurp/ncfo/opera/make-url-lists.sh
+++ net-slurp/ncfo/opera/make-url-lists.sh
@@ -6,11 +6,14 @@ INDEX_VIDEO="${3-${INDEX}}"
 rm -f *.urllist
 
 ./extract-column-links.pl "$INDEX" \
-    'Soprano Chorus MP3s' +0 'All' \
-    | sed -e '/SopHi/d' > Katarina.mp3.urllist
+    'Soprano Chorus MP3s' +0 'Meerkats' \
+    | sed -e '/SopHi/d;/OldHi/d' > Katarina.mp3.urllist
 ./extract-column-links.pl "$INDEX" \
     'Soprano Chorus MP3s' +1 'All' \
-    | sed -e '/SopHi/d' >> Katarina.mp3.urllist
+    | sed -e '/SopHi/d;/OldHi/d' >> Katarina.mp3.urllist
+./extract-column-links.pl "$INDEX" \
+    'Soprano Chorus MP3s' +2 'Meerkats' \
+    | sed -e '/SopHi/d;/OldHi/d' >> Katarina.mp3.urllist
 ~/scripts/net-slurp/plinks.pl -t "$INDEX_VIDEO" \
     | sed -e '/\.mp4$/I!d;/^[^	]*chorus/I!d;s/^[^	]*	//' \
     > Katarina.video.urllist
@@ -20,6 +23,9 @@ rm -f *.urllist
     | sed -e '/XXXnonesuchXXX/d' > bert.mp3.urllist
 ./extract-column-links.pl "$INDEX" \
     'Bass Chorus MP3s' +1 'All' \
+    | sed -e '/XXXnonesuchXXX/d' >> bert.mp3.urllist
+./extract-column-links.pl "$INDEX" \
+    'Bass Chorus MP3s' +2 'All' \
     | sed -e '/XXXnonesuchXXX/d' >> bert.mp3.urllist
 ~/scripts/net-slurp/plinks.pl -t "$INDEX_VIDEO" \
     | sed -e '/rain.*dance[^\/	]*\.mp4$/I!d;s/^[^	]*	//' \
@@ -31,6 +37,9 @@ rm -f *.urllist
 ./extract-column-links.pl "$INDEX" \
     'Alto Chorus MP3s' +1 'All' \
     | sed -e '/XXXnonesuchXXX/d' >> alto.mp3.urllist
+./extract-column-links.pl "$INDEX" \
+    'Alto Chorus MP3s' +2 'All' \
+    | sed -e '/XXXnonesuchXXX/d' >> alto_all.mp3.urllist
 
 ~/scripts/net-slurp/plinks.pl "$INDEX_PDF" \
      | sed -e '/\.pdf$/I!d;/Score/!d' > score.pdf.urllist
