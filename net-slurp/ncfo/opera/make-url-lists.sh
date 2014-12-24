1cbf5a05bff37d7e97d5186ba12d6a64cdc82974
bert <velebit@users.noreply.github.com>
2014-12-24 02:27:37 +0000

    [NCFO] Update URL lists after KC15 casting.

--- net-slurp/ncfo/opera/make-url-lists.sh
+++ net-slurp/ncfo/opera/make-url-lists.sh
@@ -9,10 +9,10 @@ rm -f *.urllist
 # MP3s
 ./extract-column-links.pl "$INDEX" \
     'Soprano Chorus MP3s' +0 'Jury Kids' \
-    | sed -e '/Sop2/d;/Story/!{;/Encore/!d;}' > Katarina.mp3.urllist
+    | sed -e '/Sop2/d' > Katarina.mp3.urllist
 ./extract-column-links.pl "$INDEX" \
     'Soprano Chorus MP3s' +1 'Jury Kids' \
-    | sed -e '/Sop2/d;/Story/!{;/Encore/!d;}' >> Katarina.mp3.urllist
+    | sed -e '/Sop2/d' >> Katarina.mp3.urllist
 ## unstructured MP3 links following the table...
 #./plinks.pl -b -pt -t -tl 1 "$INDEX" \
 #    | sed -e '/\.mp3$/I!d' \
@@ -29,10 +29,10 @@ rm -f *.urllist
 # MP3s
 ./extract-column-links.pl "$INDEX" \
     'Alto Chorus MP3s' +0 'Jury Kids' \
-    | sed -e '/XXXtextXXX/d;/Story/!{;/Encore/!d;}' > Abbe.mp3.urllist
+    | sed -e '/Barbara1.*ChorusHi/d' > Abbe.mp3.urllist
 ./extract-column-links.pl "$INDEX" \
     'Alto Chorus MP3s' +1 'Jury Kids' \
-    | sed -e '/XXXtextXXX/d;/Story/!{;/Encore/!d;}' >> Abbe.mp3.urllist
+    | sed -e '/Barbara1.*ChorusHi/d' >> Abbe.mp3.urllist
 
 ### demo MP3s
 ./plinks.pl -h -t "$INDEX" \
@@ -47,3 +47,19 @@ rm -f *.urllist
 ### score PDFs
 ./plinks.pl "$INDEX_PDF" \
      | sed -e '/\.pdf$/I!d;/Score/!d' > score.pdf.urllist
+
+### generic Jury Kids soprano I MP3s
+./extract-column-links.pl "$INDEX" \
+    'Soprano Chorus MP3s' +0 'Jury Kids' \
+    | sed -e '/Sop2/d' > 'Jury Kids soprano'.mp3.urllist
+./extract-column-links.pl "$INDEX" \
+    'Soprano Chorus MP3s' +1 'Jury Kids' \
+    | sed -e '/Sop2/d' >> 'Jury Kids soprano'.mp3.urllist
+
+### generic Jury Kids alto MP3s
+./extract-column-links.pl "$INDEX" \
+    'Alto Chorus MP3s' +0 'Jury Kids' \
+    | sed -e '/XXXtextXXX/d' > 'Jury Kids alto'.mp3.urllist
+./extract-column-links.pl "$INDEX" \
+    'Alto Chorus MP3s' +1 'Jury Kids' \
+    | sed -e '/XXXtextXXX/d' >> 'Jury Kids alto'.mp3.urllist
