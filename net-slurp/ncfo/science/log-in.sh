cf12c57a301e393aa5d620a06b7fa84438cfc014
bert <velebit@users.noreply.github.com>
2015-02-07 08:26:37 +0000

    [NCFO] Update chorus log-in script for 2015.

--- net-slurp/ncfo/science/log-in.sh
+++ net-slurp/ncfo/science/log-in.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-#PAGE_DIR=node/197
-PAGE_DIR=2014_Powers_of_Ten_Practice_MP3s
+#page=node/197
+#page=2014_Powers_of_Ten_Practice_MP3s
+page=welcome
+destination='?destination=node/52'
+
 wget --save-cookies cookies.txt \
     --post-data 'name=ncfo-chorus&pass='"`cat .pw`"'&op=Log%20in&form_build_id=form-e07efccc46c8bdf81109e1eed6ecaa8d&form_id=user_login_block' \
     -O /dev/null \
-    http://www.familyopera.org/drupal/"$PAGE_DIR"/
+    http://www.familyopera.org/drupal/"$page""$destination"
