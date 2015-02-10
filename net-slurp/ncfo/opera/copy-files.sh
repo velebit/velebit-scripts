e831b07288b39ae38537893ea298c3175fb27f4f
bert <velebit@users.noreply.github.com>
2015-02-10 16:39:50 +0000

    [NCFO] Set canonical file names for the science chorus.
    Also updated the copy-files script for opera so it matches, but
    there were no changes in behavior for that one.

--- net-slurp/ncfo/opera/copy-files.sh
+++ net-slurp/ncfo/opera/copy-files.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-PF_ARGS=
+PF_ARGS=()
 while true; do
     case "$1" in
 	-f|--fast)
-	    PF_ARGS='--no-gain --no-wipe'; shift ;;
+	    PF_ARGS=('--no-gain --no-wipe'); shift ;;
 	-*)
 	    echo "Unknown flag '$1'!" >&2 ; exit 1 ;;
 	*)
@@ -12,8 +12,10 @@ while true; do
     esac
 done
 
+CF_ARGS=()
+
 inspect() {
-    #tee "$1"
+    #tee "log-$1.tmp"
     cat
 }
 
@@ -21,9 +23,9 @@ inspect() {
 ./urllist2process.pl *.mp3.urllist | inspect M1 \
     | ./extras2process.pl mp3-extras.* | inspect M2 \
     | ./gain-cache.pl | inspect M3 \
-    | ./canonicalize-filenames.pl | inspect M4 \
-    | ./process-files.pl $PF_ARGS
+    | ./canonicalize-filenames.pl "${CF_ARGS[@]}" | inspect M4 \
+    | ./process-files.pl "${PF_ARGS[@]}"
 ./urllist2process.pl -s video/ -d ../video/ *.video.urllist | inspect V1 \
-    | ./process-files.pl $PF_ARGS
+    | ./process-files.pl "${PF_ARGS[@]}"
 ./playlists.sh
 ./id3-tags.sh
--- net-slurp/ncfo/science/copy-files.sh
+++ net-slurp/ncfo/science/copy-files.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-PF_ARGS=
+PF_ARGS=()
 while true; do
     case "$1" in
 	-f|--fast)
-	    PF_ARGS='--no-gain --no-wipe'; shift ;;
+	    PF_ARGS=('--no-gain --no-wipe'); shift ;;
 	-*)
 	    echo "Unknown flag '$1'!" >&2 ; exit 1 ;;
 	*)
@@ -12,9 +12,19 @@ while true; do
     esac
 done
 
+CF_ARGS=(-rf canonical-replacements.txt \
+    --no-replace-any-prefix --fallback-prefix LLMzz_)
+
+inspect() {
+    #tee "log-$1.tmp"
+    cat
+}
+
 ./make-url-lists.sh
-./urllist2process.pl *.mp3.urllist | ./extras2process.pl mp3-extras.* \
-    | ./gain-cache.pl | ./canonicalize-filenames.pl \
-    | ./process-files.pl $PF_ARGS
+./urllist2process.pl *.mp3.urllist | inspect M1 \
+    | ./extras2process.pl mp3-extras.* | inspect M2 \
+    | ./gain-cache.pl | inspect M3 \
+    | ./canonicalize-filenames.pl "${CF_ARGS[@]}" | inspect M4 \
+    | ./process-files.pl "${PF_ARGS[@]}"
 ./playlists.sh
 ./id3-tags.sh
