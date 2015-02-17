26b7d5097b7ac0f57782de6b303c404bd77450b4
bert <velebit@users.noreply.github.com>
2015-02-17 03:51:28 +0000

    [NCFO] Update chorus URL lists.

--- net-slurp/ncfo/science/make-url-lists.sh
+++ net-slurp/ncfo/science/make-url-lists.sh
@@ -6,6 +6,8 @@ DIR=urllists
 rm -f *.urllist "$DIR"/*.urllist *.tmplist "$DIR"/*.tmplist
 if [ ! -d "$DIR" ]; then mkdir "$DIR"; fi
 
+### generate simple URL lists
+
 FULL="$DIR/raw0.tmplist"
 #./plinks.pl -b -pt -t "$INDEX" > "$FULL"
 ./plinks.pl -b -lt -t "$INDEX" > "$FULL"
@@ -14,69 +16,89 @@ for voice in soprano alto tenor bass; do
     sed -e '/^'"$voice"'.*\.mp3$/I!d' \
 	-e 's/^[^	]*	//' "$FULL" > "$LEVEL1"
 
-#    for size in mob small; do
-#	LEVEL2="$DIR/raw2-$voice-$size.tmplist"
-#	case "$size" in
-#	    mob)
-#		sed -e '/^[^	]*small group/Id' \
-#		    -e 's/^[^	]*	//' "$LEVEL1" > "$LEVEL2" ;;
-#	    small)
-#		sed -e 's/^[^	]*	//' "$LEVEL1" > "$LEVEL2" ;;
-#	esac
-
     # just strip the line info
     LEVEL2="$DIR/raw2-$voice.tmplist"
     sed -e 's/^[^	]*	//' "$LEVEL1" > "$LEVEL2"
 
-#	for age in kids adults; do
-#	    case "$voice-$age" in
-#		tenor-kids|bass-kids)  continue ;;
-#	    esac
-#	    LEVEL3="$DIR/raw3-$voice-$age-$size.tmplist"
-#	    case "$age" in
-#		kids)
-#		    sed -e '/^[^	]*adults/Id' \
-#			-e 's/^[^	]*	//' "$LEVEL2" > "$LEVEL3" ;;
-#		adults)
-#		    sed -e '/^[^	]*kids/Id' \
-#			-e 's/^[^	]*	//' "$LEVEL2" > "$LEVEL3" ;;
-#	    esac
+    for age in adults kids; do
+	LEVEL3="$DIR/raw3-$voice.tmplist"
+	case "$voice-$age" in
+	    tenor-kids|bass-kids)
+		      continue ;;
+	    *-adults) skip=kids ;;
+	    *-kids)   skip=adults ;;
+	esac
+	sed -e '/^'"$skip"'	/Id' \
+	    -e 's/^[^	]*	//' "$LEVEL2" > "$LEVEL3"
 
-    # just strip the link text info
-    LEVEL3="$DIR/raw3-$voice.tmplist"
-    sed -e 's/^[^	]*	//' "$LEVEL2" > "$LEVEL3"
-
-    ### OK, generate the URL list
-    URLLIST="$DIR/$voice-all.mp3.urllist"
-    cp "$LEVEL3" "$URLLIST"
-
-#	    ### OK, generate the URL list
-#	    URLLIST="$DIR/$voice-$age-$size.mp3.urllist"
-#	    cp "$LEVEL3" "$URLLIST"
-#
-#	    # program-specific fixups
-#	    FIXLIST="$DIR/fix.tmplist"
-#	    case "$age-$size" in
-#		kids-small)
-#		    sed -e '/string.*kids/Id' "URLLIST" > "$FIXLIST"
-#		    mv "$FIXLIST" "$URLLIST"
-#		    ;;
-#	    esac
-#	done
-#    done
-
+	# OK, generate the URL list
+	URLLIST="$DIR/$voice-$age-all.mp3.urllist"
+	cp "$LEVEL3" "$URLLIST"
+    done
 done
+
+sed -e '/^demo.*\.mp3$/I!d' \
+    -e 's/^[^	]*	//' -e 's/^[^	]*	//' \
+    -e 's/^[^	]*	//' "$FULL" > "$DIR"/demo.mp3.urllist
+
+#sed -e '/\.pdf$/I!d' \
+#    -e 's/^[^	]*	//' -e 's/^[^	]*	//' \
+#    -e 's/^[^	]*	//' \
+#    -e '/Score/!d' "$FULL" > "$DIR"/score.pdf.urllist
+
 rm -f *.tmplist "$DIR"/*.tmplist
 
-./plinks.pl -b "$INDEX" \
-     | sed -e '/^demo.*\.mp3$/I!d;s/^[^	]*	//' > "$DIR"/demo.mp3.urllist
+### generate generic URL lists
 
-./plinks.pl "$INDEX" \
-     | sed -e '/\.pdf$/I!d;/Score/!d' > "$DIR"/score.pdf.urllist
+for age in adults kids; do
+    va="soprano-$age"
+    sed -e '/Medley\|Cosmic\|Laser\|Sky.*Dance/I{;/high/I!d;}' \
+	-e '/Straight/{;/mezzo/Id;}' \
+	-e '/Straight/{;/mezzo/Id;}' \
+	"$DIR"/"$va"-all.mp3.urllist > "$DIR"/"$va"-high.mp3.urllist
+    sed -e '/Medley\|Cosmic\|Laser\|Sky.*Dance/I{;/high/Id;}' \
+	-e '/Straight/{;/mezzo/I!d;}' \
+	"$DIR"/"$va"-all.mp3.urllist > "$DIR"/"$va"-low.mp3.urllist
+done
 
-ln -s "$DIR"/soprano-all.mp3.urllist Katarina+Luka.mp3.urllist
-ln -s "$DIR"/alto-all.mp3.urllist Abbe.mp3.urllist
-ln -s "$DIR"/tenor-all.mp3.urllist bert.mp3.urllist
+for age in adults kids; do
+    va="alto-$age"
+    sed -e '/Medley\|Cosmic\|Laser\|Sky.*Dance/I{;/low/Id;}' \
+	"$DIR"/"$va"-all.mp3.urllist > "$DIR"/"$va"-high.mp3.urllist
+    sed -e '/Medley\|Cosmic\|Laser\|Sky.*Dance/I{;/low/I!d;}' \
+	"$DIR"/"$va"-all.mp3.urllist > "$DIR"/"$va"-low.mp3.urllist
+done
+
+sed -e '/Laser\|Sky.*Dance/I{;/high/I!d;}' \
+    "$DIR"/tenor-adults-all.mp3.urllist > "$DIR"/tenor-high.mp3.urllist
+sed -e '/Laser\|Sky.*Dance/I{;/high/Id;}' \
+    "$DIR"/tenor-adults-all.mp3.urllist > "$DIR"/tenor-low.mp3.urllist
+
+sed -e '/Cosmic\|Sky.*Dance/I{;/low/Id;}' \
+    -e '/Laser/I{;/high/I!d;}' \
+    "$DIR"/bass-adults-all.mp3.urllist > "$DIR"/bass-high.mp3.urllist
+# "mid" can go with either low or high in two-split songs:
+sed -e '/Cosmic\|Sky.*Dance/I{;/low/Id;}' \
+    -e '/Laser/I{;/mid/I!d;}' \
+    "$DIR"/bass-adults-all.mp3.urllist > "$DIR"/bass-mid-hi.mp3.urllist
+sed -e '/Cosmic\|Sky.*Dance/I{;/low/I!d;}' \
+    -e '/Laser/I{;/mid/I!d;}' \
+    "$DIR"/bass-adults-all.mp3.urllist > "$DIR"/bass-mid-lo.mp3.urllist
+sed -e '/Cosmic\|Sky.*Dance/I{;/low/I!d;}' \
+    -e '/Laser/I{;/low/I!d;}' \
+    "$DIR"/bass-adults-all.mp3.urllist > "$DIR"/bass-low.mp3.urllist
+
+### specific URL lists for our family
+
+cp "$DIR"/soprano-kids-low.mp3.urllist Katarina+Luka.mp3.urllist
+
+sed -e '/NothingForNow/Id' \
+    "$DIR"/alto-adults-low.mp3.urllist > Abbe.mp3.urllist
+
+sed -e '/NothingForNow/Id' \
+    "$DIR"/tenor-low.mp3.urllist > bert.mp3.urllist
+sed -e '/NothingForNow/I!d' \
+    "$DIR"/bass-high.mp3.urllist >> bert.mp3.urllist
 
 #sed -e '/Fingers/Id' \
 #    "$DIR"/alto-adults-small.mp3.urllist > Abbe.mp3.urllist
@@ -93,6 +115,11 @@ ln -s "$DIR"/demo.mp3.urllist demo.mp3.urllist
 #ln -s "$DIR"/score.pdf.urllist score.pdf.urllist
 
 ## other people:
+
+for vap in soprano-adults-low alto-adults-high; do
+    file="$vap".mp3.urllist
+    ln -s "$DIR"/"$file" X-"$file"
+done
 
 #for v in soprano alto; do
 #    file="$v"-kids-mob.mp3.urllist
