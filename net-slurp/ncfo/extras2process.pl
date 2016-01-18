e764354519ad08cd0bf2d125762c682074d30e49
bert <velebit@users.noreply.github.com>
2016-01-18 04:19:29 +0000

    [NCFO] extras2process: Add "before/after" options.

--- net-slurp/ncfo/extras2process.pl
+++ net-slurp/ncfo/extras2process.pl
@@ -1,11 +1,19 @@
 #!/usr/bin/perl
 #
-# usage: extras2process [dirs...] [-- input-files...]
+# usage: extras2process [dirs...] [-- directive-files...]
 #
 # Takes a list of extras directories on the command line, and a list
-# of already prepared processing directives on stdin.  The extras
-# directories must be named "*.DEST" or "DEST", where DEST is the
-# actual destination directory for the extras.
+# of already prepared processing directives on stdin.  The names of
+# the extras directories must be in one of the following formats:
+#     **/*.DEST/first
+#     **/*.DEST/before.MATCH
+#     **/*.DEST/after.MATCH
+#     **/*.DEST/last
+#     **/*.DEST
+# where "DEST" is the actual destination directory for the extras
+# (which must not include periods), "MATCH" is a string to match in
+# the input directives, "*." is an optional string prefix and "**/" is
+# an optional path.  For files in *.DEST, "last" is implied.
 #
 # The output is all of the directives seen on stdin, *plus* all of the
 # corresponding extras.  Extras for directories not seen on stdin will
@@ -15,41 +23,85 @@ use warnings;
 use strict;
 
 my $SORTED = 0;
-my @dir_entries;
+my (@match_before_entries, @match_after_entries);
+
+sub file ( $ ) { my ($f) = @_;  $f =~ s,.*/,,;  $f; }
+
 while (@ARGV) {
   my $dir = shift @ARGV;
   ($dir eq '--sort' or $dir eq '-s') and $SORTED = 1, next;
   $dir eq '--' and last;
-  my @files = glob "$dir/*";
-  s,.*/,, for @files;
-  my $name = $dir;  $name =~ s,.*/,,;  $name =~ s,.*\.,,;
-  my ($entry) = grep $_->[0] eq $name, @dir_entries;
-  if (! defined $entry) {
-    push @dir_entries, [$name, []];
-    ($entry) = grep $_->[0] eq $name, @dir_entries
-      or die "internal error";
+  $dir =~ /-gain$/ and next;  # skip gain cache directories
+  my $dest = $dir;  $dest =~ s,.*/,,;  $dest =~ s,.*\.,,;
+  {
+    my @files = grep -f, glob "$dir/first/*";
+    push @match_before_entries, [qr!=\.\./\Q$dest\E/!,
+				 [map "$_=../$dest/" . file($_), @files]]
+      if @files;
   }
-  push @{$entry->[1]}, map "$dir/$_=../$name/$_", @files;
+  for my $d (grep -d, glob "$dir/before.*") {
+    my $re = join '.*', split /\+/, ((split /\./, file($d), 2)[1]);
+    my @files = grep -f, glob "$d/*";
+    push @match_before_entries, [qr!=\.\./\Q$dest\E/.*$re!,
+				 [map "$_=../$dest/" . file($_), @files]]
+      if @files;
+  }
+  for my $d (grep -d, glob "$dir/after.*") {
+    my $re = join '.*', split /\+/, ((split /\./, file($d), 2)[1]);
+    my @files = grep -f, glob "$d/*";
+    push @match_after_entries, [qr!=\.\./\Q$dest\E/.*$re!,
+				 [map "$_=../$dest/" . file($_), @files]]
+      if @files;
+  }
+  {
+    my @files = grep -f, glob "$dir/last/*";
+    push @match_after_entries, [qr!=\.\./\Q$dest\E/!,
+				[map "$_=../$dest/" . file($_), @files]]
+      if @files;
+  }
+  {
+    my @files = grep -f, glob "$dir/*";
+    push @match_after_entries, [qr!=\.\./\Q$dest\E/!,
+				[map "$_=../$dest/" . file($_), @files]]
+      if @files;
+  }
 }
 
 my @entries = <>;
 s/\r?\n$// for @entries;
 
+sub in_path ( $ ) { (split /=/, $_[0], 2)[0]; }
 sub out_path ( $ ) { (split /=/, $_[0], 2)[1]; }
 
-my @out_paths = map out_path($_), @entries;
-
-my @extra_entries;
-for my $entry (@dir_entries) {
-  my $name = $entry->[0];
-  if (scalar grep $_ =~ m!/\Q$name\E/!, @out_paths) {
-    push @extra_entries, @{$entry->[1]};
-  } else {
-    print STDERR "Skipped extra: $_\n"
-      for map out_path($_), @{$entry->[1]};
+sub add_matches_before ( $$ ) {
+  my ($entries, $match_entries) = @_;
+  my @results;
+  for my $entry (@$entries) {
+  MATCH:
+    for my $match (@$match_entries) {
+      my $re = $match->[0];
+      defined $re or next MATCH;
+      if ($entry =~ m!$re!) {
+	push @results, @{$match->[1]};
+	$match->[0] = undef;
+      }
+    }
+    push @results, $entry;
   }
+  @results;
 }
 
-push @entries, @extra_entries;
+sub add_matches_after ( $$ ) {
+  my ($entries, $match_entries) = @_;
+  reverse add_matches_before [reverse @$entries], $match_entries;
+}
+
+
+@entries = add_matches_before(\@entries, \@match_before_entries);
+@entries = add_matches_after(\@entries, \@match_after_entries);
 @entries = sort { out_path($a) cmp out_path($b) } @entries if $SORTED;
 print "$_\n" for @entries;
+
+print STDERR "Skipped extra file: $_\n"
+  for map out_path($_), map @{$_->[1]},
+    grep defined($_->[0]), (@match_before_entries, @match_after_entries);
