28c01c04d36d229d25f7feaa6b8f5bd3b4201174
bert <velebit@users.noreply.github.com>
2018-01-07 00:32:23 +0000

    [NCFO] mix: Better canonicalize subdir names.

--- net-slurp/ncfo/opera/pre-mix.sh
+++ net-slurp/ncfo/opera/pre-mix.sh
@@ -4,10 +4,16 @@
 
 source=bert.mp3.urllist
 
-sed -e 's/.*	out_file://;s/^[SATB] //;s/ \?- rev .*//I' "$source" \
+sed -e 's/.*	out_file://;s/^[SATB] //' \
+    -e 's/ \?- rev .* \(bars \)/ \1/I;s/ \?- rev .*//I' \
+    -e 's/,\? \( high\| low\)\?tenor\( [12]\)\?//I' \
+    "$source" \
+    | sort | uniq \
     | while read track; do
+	  pattern=$(echo "$track" \
+	      | sed -e 's/ \(bars \)/.* \1/I' )
           ./urllist2process.pl X-all-voices.mp3.urllist \
-              | fgrep "$track" \
+              | grep -i "$pattern" \
 	      | sed -e 's,=.*/,=mix/'"$track"'/,'
       done \
     | ./process-files.pl
