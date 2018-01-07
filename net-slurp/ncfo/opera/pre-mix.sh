421c3de88374fc2a3ff34c0e1ebdff181ac1aade
bert <velebit@users.noreply.github.com>
2018-01-07 12:17:04 +0000

    [NCFO] pre-mix: Stage "non-bert" mergeable tracks.

--- net-slurp/ncfo/opera/pre-mix.sh
+++ net-slurp/ncfo/opera/pre-mix.sh
@@ -2,11 +2,13 @@
 
 ./make-url-lists.sh
 
-source=bert.mp3.urllist
+source=X-all-voices.mp3.urllist
 
 sed -e 's/.*	out_file://;s/^[SATB] //' \
     -e 's/ \?- rev .* \(bars \)/ \1/I;s/ \?- rev .*//I' \
-    -e 's/,\? \( high\| low\)\?tenor\( [12]\)\?//I' \
+    -e 's/,\? \(high \|low \)\?\(soprano\|alto\|tenor\|bass\)\( [12]\)\?//I' \
+    -e 's/,\? \(hi\(gh\)\? \|middle \|low \)\?split//I' \
+    -e '/KCCC/Id' \
     "$source" \
     | sort | uniq \
     | while read track; do
@@ -14,6 +16,6 @@ sed -e 's/.*	out_file://;s/^[SATB] //' \
 	      | sed -e 's/ \(bars \)/.* \1/I' )
           ./urllist2process.pl X-all-voices.mp3.urllist \
               | grep -i "$pattern" \
-	      | sed -e 's,=.*/,=mix/'"$track"'/,'
+              | sed -e 's@=.*/@=mix-sources/'"$track"'/@'
       done \
     | ./process-files.pl
