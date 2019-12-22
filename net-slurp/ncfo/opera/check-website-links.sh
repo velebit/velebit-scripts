be6e617837a5b29eecd0707c4db311c92a2657a9
bert <velebit@users.noreply.github.com>
2019-12-22 18:23:03 +0000

    [NCFO] Completely rewrite checking of site links.

--- net-slurp/ncfo/opera/check-website-links.sh
+++ net-slurp/ncfo/opera/check-website-links.sh
@@ -2,49 +2,27 @@
 
 source "$(dirname "$0")/_uri.sh"
 
-type=check-links
-file_dir=check-links
-log=download-"$type".log
-
 all_uris=("$chorus_uri" "$solo_uri" "$demo_uri" "$pdf_uri" "$video_uri")
 
-for file in "${all_uris[@]##*/}"; do
-    if [ -f "$html_dir/$file.html" ]; then
-        rm -f "$html_dir/$file"
-        mv "$html_dir/$file.html" "$html_dir/$file"
-    fi
-    cp -p "$html_dir/$file" "$html_dir/$file".orig
+rm -f check-links.frag.log check-links.log
+for uri in "${all_uris[@]}"; do
+    ./log-in.sh
+    wget --load-cookies cookies.txt \
+	 -P check-links -x -N --restrict-file-names=windows \
+	 --wait=1 -nv \
+	 "$uri" \
+	 2>&1 | tee check-links.frag.log
+    cat check-links.frag.log >> check-links.log
+    wget --load-cookies cookies.txt \
+	 -P check-links -x -N --restrict-file-names=windows \
+	 --wait=0.25 -nv --spider \
+	 -i "check-links/${uri#http://}" --force-html --base="$uri" \
+	 2>&1 | tee check-links.frag.log
+    cat check-links.frag.log >> check-links.log
 done
-if ! wget --load-cookies cookies.txt \
-    -nd -P "$html_dir" -N --restrict-file-names=windows \
-    --progress=bar:force \
-    "${all_uris[@]}" \
-  > download-index.log 2>&1; then
-    cat download-index.log
-    for file in "${all_uris[@]##*/}"; do
-        rm -f "$html_dir/$file"
-        mv "$html_dir/$file".orig "$html_dir/$file.html"
-    done
-    exit 1
-fi
-cat download-index.log
-for file in "${all_uris[@]##*/}"; do
-    rm -f "$html_dir/$file".orig
-    rm -f "$html_dir/$file.html"; mv "$html_dir/$file" "$html_dir/$file.html"
-done
+rm -f check-links.frag.log
 
-./make-url-lists.sh --check-links \
-                    --chorus "$html_dir/${chorus_uri##*/}.html" \
-                    --solo "$html_dir/${solo_uri##*/}.html" \
-                    --demo "$html_dir/${demo_uri##*/}.html" \
-                    --orch "$html_dir/${demo_uri##*/}.html" \
-                    --pdf "$html_dir/${pdf_uri##*/}.html" \
-                    --video "$html_dir/${video_uri##*/}.html"
-sed -e 's/	.*//' *.urllist | sort | uniq \
-    > "$type"-master.urllist
-wget --load-cookies cookies.txt -i "$type"-master.urllist \
-    -nd -P "$file_dir" -N --restrict-file-names=windows \
-    --progress=bar:force \
-  2>&1 | tee "$log"
-rm -f "$type"-master.urllist
-./clean-up.pl -d "$file_dir" "$log"
+echo ''; echo =====
+grep 'unable to resolve' check-links.log
+echo --
+grep -B1 'ERROR\|broken link!' check-links.log
