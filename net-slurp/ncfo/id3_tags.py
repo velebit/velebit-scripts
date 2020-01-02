da5b14b6dfe144bfc774b83815362b634250bc33
bert <velebit@users.noreply.github.com>
2020-01-02 05:18:17 +0000

    [NCFO] id3_tags: Add back parallelism.
    This greatly improves performance.

--- net-slurp/ncfo/id3_tags.py
+++ net-slurp/ncfo/id3_tags.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 import argparse
-import contextlib
+import eyed3
+import eyed3.id3
 import glob
+import multiprocessing
 import os
 import os.path
 import re
@@ -55,19 +57,19 @@ def parse_args():
                        action='store_const', dest='id3_track_strip',
                        const=strip_number, default=None)
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-W', '--wipe',
-                       action='store_const', dest='old_tag_args',
-                       const=('--remove-all',), default=None)
-    group.add_argument('-k', '--keep',
-                       action='store_const', dest='old_tag_args',
-                       const=(), default=None)
+    group.add_argument('-W', '--wipe', action='store_const',
+                       dest='remove_version', default=eyed3.id3.ID3_V1,
+                       const=eyed3.id3.ID3_ANY_VERSION)
+    group.add_argument('-k', '--keep', action='store_const',
+                       dest='remove_version', default=eyed3.id3.ID3_V1,
+                       const=None)
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-3', '--2.3',
-                       action='store_const', dest='new_tag_args',
-                       const=('--to-v2.3',), default=None)
-    group.add_argument('-4', '--2.4',
-                       action='store_const', dest='new_tag_args',
-                       const=('--to-v2.4',), default=None)
+    group.add_argument('-3', '--2.3', action='store_const',
+                       dest='out_version', default=eyed3.id3.ID3_V2_3,
+                       const=eyed3.id3.ID3_V2_3)
+    group.add_argument('-4', '--2.4', action='store_const',
+                       dest='out_version', default=eyed3.id3.ID3_V2_3,
+                       const=eyed3.id3.ID3_V2_4)
     parser.add_argument('-d', '--directory', action='append',
                         dest='dir_prefix', default=[], metavar='DIR')
     parser.add_argument('-P', '--num-processes', action='store', type=int,
@@ -80,10 +82,6 @@ def parse_args():
         settings.id3_playlist_strip = make_keep_word(2)
     if settings.id3_track_strip is None:
         settings.id3_track_strip = keep_all
-    if settings.old_tag_args is None:
-        settings.old_tag_args=('--remove-v1',)
-    if settings.new_tag_args is None:
-        settings.new_tag_args=('--to-v2.3',)
     settings.dir_prefix = ''.join([d+"/" for d in settings.dir_prefix])
 
     return settings
@@ -106,11 +104,55 @@ def keep_paren(text):
 def strip_number(text):
     return re.sub(r'^[0-9][0-9]*[ _-]?', '', text)
 
-def skip_error_line(line, settings):
-    return (settings.verbose < 1 and
+def skip_error_line(line, verbose):
+    return (verbose < 1 and
             re.search(r"Frame 'RVA2' is not yet supported", line))
 
-def update_tags_from_playlist(playlist, settings, logfile=None):
+def get_tag_args(remove_version, out_version):
+    if remove_version is None:
+        remove_args = ()
+    elif remove_version == eyed3.id3.ID3_V1:
+        remove_args = ('--remove-v1',)
+    elif remove_version == eyed3.id3.ID3_V2:
+        remove_args = ('--remove-v2',)
+    elif remove_version == eyed3.id3.ID3_ANY_VERSION:
+        remove_args = ('--remove-all',)
+    else:
+        print("Unknown remove_version ", remove_version)
+        remove_args = ()
+    if out_version == eyed3.id3.ID3_V2_3:
+        version_args = ('--to-v2.3',)
+    elif out_version == eyed3.id3.ID3_V2_4:
+        version_args = ('--to-v2.4',)
+    else:
+        print("Unknown out_version ", out_version)
+        version_args = ('--to-v2.3',)
+    return remove_args + version_args
+
+def update_file_tags(data):
+    with open(data['log'], 'a') as logfile:
+        print(f"Updating tags for {data['file']}...", file=logfile)
+        cmd=(('eyeD3', '-Q') +
+             get_tag_args(data['remove_version'], data['out_version']) +
+             ('-t', data['name'], '-a', data['artist'],
+              '-A', data['album'],
+              '-n', data['track'], '-N', data['num_tracks'],
+              '--no-color', '--preserve-file-times',
+              data['file']))
+        result = subprocess.run(
+            cmd, check=False, encoding='UTF-8',
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(result.stdout, end='', file=logfile)
+        print(result.stderr, end='', file=logfile)
+        if data['verbose'] >= 2:
+            print(result.stdout, end='', file=sys.stdout)
+        for line in re.split(r'(?<=\n)', result.stderr):
+            if not skip_error_line(line, data['verbose']):
+                print(line, end='', file=sys.stderr)
+
+command_queue = []
+
+def queue_tags_from_playlist(playlist, settings, log=os.devnull):
     who = os.path.splitext(os.path.basename(playlist))[0]
     who = settings.id3_playlist_strip(who)
     who = re.sub(r'^X-X*', '', who, count=1)
@@ -134,40 +176,51 @@ def update_tags_from_playlist(playlist, settings, logfile=None):
         file = tracks[i]
         name = os.path.splitext(os.path.basename(file))[0]
         name = settings.id3_track_strip(name)
-        if logfile is not None:
-            print(f"Updating tags for {file}...", file=logfile)
-        cmd=(('eyeD3',) + settings.old_tag_args + settings.new_tag_args +
-             ('-t', name, '-a', settings.id3_artist, '-A', album,
-              '-n', track, '-N', num_tracks, '--no-color', '-Q',
-              '--preserve-file-times',
-              file))
+        data = { 'file': file, 'name': name, 'artist': settings.id3_artist,
+                 'album': album, 'track': track, 'num_tracks': num_tracks,
+                 'remove_version': settings.remove_version,
+                 'out_version': settings.out_version,
+                 'log': log, 'verbose': settings.verbose }
         if settings.dry_run:
-            print(" ".join(("WOULD run:", *cmd)))
+            print(f"WOULD update tags for {file}")
         else:
-            result = subprocess.run(
-                cmd, check=False, encoding='UTF-8',
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            if settings.verbose >= 2:
-                print(result.stdout, end='', file=sys.stdout)
-            for line in re.split(r'(?<=\n)', result.stderr):
-                if not skip_error_line(line, settings):
-                    print(line, end='', file=sys.stderr)
-            if logfile is not None:
-                print(result.stdout, end='', file=logfile)
-                print(result.stderr, end='', file=logfile)
+            global command_queue
+            command_queue.append(data)
+
+def process_queue():
+    global command_queue
+
+    try:
+        num_available_cores = len(os.sched_getaffinity(0))
+        num_processes = int(num_available_cores * 1.5 + 0.01)
+    except:
+        print("Could not get available cores, defaulting to 1 process!",
+              file=sys.stderr)
+        num_processes = 1
+
+    print("Processing tags...")
+    if num_processes == 1:
+        for data in command_queue:
+            update_file_tags(data)
+    else:
+        with multiprocessing.Pool(processes=num_processes) as pool:
+            pool.map(update_file_tags, command_queue)
+            pool.close()
 
 def default_playlists():
     return [p for p in sorted(glob.glob('*.m3u'))
             if not re.search(r'(?:^|\s)X-', p)]
 
 def process_playlist(playlist, settings):
-    print(f"Updating tags for playlist {settings.dir_prefix}{playlist}...")
+    print(f"Preparing tags for playlist {settings.dir_prefix}{playlist}...")
     log = 'id3_tags.' + os.path.basename(playlist) + '.log'
-    with open(log, 'w') as logfile:
-        update_tags_from_playlist(playlist, settings, logfile=logfile)
+    try:
+        os.unlink(log)
+    except FileNotFoundError:
+        pass
+    queue_tags_from_playlist(playlist, settings, log=log)
 
 def process_playlist_args(args, settings):
-    # TODO add back parallelism
     for playlist in args:
         process_playlist(playlist, settings)
 
@@ -183,6 +236,7 @@ def main():
         process_playlist_args(default_playlists(), settings)
     else:
         process_playlist_args(settings.playlists, settings)
+    process_queue()
 
 if __name__ == "__main__":
     main()
