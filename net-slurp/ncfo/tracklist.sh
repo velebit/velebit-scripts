35a2e1f66f1b6e0c46b9c1ff0284e85922b3fdde
bert <velebit@users.noreply.github.com>
2020-02-04 03:08:37 +0000

    [NCFO] tracklist: Add support for --cue and -d.

--- net-slurp/ncfo/tracklist.sh
+++ net-slurp/ncfo/tracklist.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+CUE_SHEET=
 SORT_BY_NUMBER=()
 SORT_BY_NAME=()
 STRIP_PREFIX=()
 REPLACE_SPACES=()
+NUMBERING=()
 while true; do
     case "$1" in
 	-s|--sort)
@@ -15,6 +17,10 @@ while true; do
 	    STRIP_PREFIX=(cat); shift ;;
 	-w|--keep-word-separators)
 	    REPLACE_SPACES=(cat); shift ;;
+	-d|--decimate)
+	    NUMBERING=(decimate_and_number "$2"); shift; shift ;;
+	--cue|--cue-sheet)
+	    CUE_SHEET=yes; shift ;;
 	-?*)
 	    echo "Unknown flag '$1'!" >&2 ; exit 1 ;;
 	*)
@@ -27,6 +33,10 @@ if [ -n "${SORT_BY_NUMBER[*]}" -a -n "${SORT_BY_NAME[*]}" ]; then
 fi
 if [ -n "${SORT_BY_NAME[*]}" -a -n "${STRIP_PREFIX[*]}" ]; then
     echo "Error: --sort-name (-S) and --no-strip-prefix (-p) are mutually exclusive!" >&2
+    exit 1
+fi
+if [ -n "$CUE_SHEET" -a -n "${NUMBERING[*]}" ]; then
+    echo "Error: --cue and --decimate (-d) are mutually exclusive!" >&2
     exit 1
 fi
 
@@ -43,6 +53,31 @@ strip_prefix () {
 
 strip_part () {
     perl -pe 's/^[SATB](?:mix)? //'
+}
+
+canonicalize_name () {
+    #perl -pe 's/^pan /panned: /;s, w , w/ ,;s/\bsc(\d\S*)/Scene \1:/'
+    perl -pe 's/^pan //;s/,? w /, panned: /;s/^sc(\d\S*)/Scene \1:/'
+}
+
+decimate_and_number () {
+    local decimate="$1"; shift
+    local num=0
+    local last=0
+    local line
+    while IFS='' read -r line; do
+	num=$(("$num"+1))
+	if [ "($decimate)" = "(1)" ]; then
+	    last="$num"
+	    printf '%2d %s\n' "$num" "$line"
+	elif [ "$(((num-1) % decimate))" -eq 0 ]; then
+	    last=$((num+decimate-1))
+	    printf '%2d-%-2d %s\n' "$num" "$last" "$line"
+	fi
+    done
+    if [ "$num" -ne "$last" ]; then
+	echo "Warning: expected $last tracks, found $num." >&2
+    fi
 }
 
 show_tracks () {
@@ -66,11 +101,54 @@ show_tracks () {
     done
 }
 
-show_tracks "$@" \
-    | sed -e 's,.*/,,;s/\.wav$//i;s/\.mp3$//i' \
-    | ${SORT_BY_NUMBER[@]:-cat} \
-    | ${STRIP_PREFIX[@]:-strip_prefix} \
-    | strip_part \
-    | ${SORT_BY_NAME[@]:-cat} \
-    | ${REPLACE_SPACES[@]:-sed -e 's/[-_][-_]*/ /g'} \
-    | cat -n
+cue_header () {
+    echo "PERFORMER \"NCFO practice\""
+    echo "TITLE \"$(./canonicalize-filenames.pl -ps) practice\""
+}
+cue_track () {
+    local file="$1"; shift
+    local type="$1"; shift
+    local name="$1"; shift
+    local track="$1"; shift
+    printf "FILE \"%s\" %s\n" "$file" "$type"
+    printf "  TRACK %02d %s\n" "$track" AUDIO
+    printf "    TITLE \"%s\"\n" "$name"
+    echo   "    PERFORMER \"NCFO practice\""
+    echo   "    INDEX 01 00:00:00"
+}
+
+declare -A cue_pos
+
+make_cue_sheet () {
+    local file
+    while IFS='' read -r file; do
+	local dir="$(dirname "$file")"
+	local pretty="$(echo "$file" | process_tracks)"
+	if [ -z "${cue_pos[$dir]}" ]; then
+	    cue_pos[$dir]=0
+	    cue_header > "$dir"/tracks.cue
+	fi
+	cue_pos[$dir]=$(("${cue_pos[$dir]}"+1))
+	cue_track "$(basename "$file")" MP3 "$pretty" "${cue_pos[$dir]}" \
+		  >> "$dir"/tracks.cue
+    done
+}
+
+process_tracks () {
+    sed -e 's,.*/,,;s/\.wav$//i;s/\.mp3$//i' \
+	| ${SORT_BY_NUMBER[@]:-cat} \
+	| ${STRIP_PREFIX[@]:-strip_prefix} \
+	| strip_part \
+	| canonicalize_name \
+	| ${SORT_BY_NAME[@]:-cat} \
+	| ${REPLACE_SPACES[@]:-sed -e 's/[-_][-_]*/ /g'}
+}
+
+if [ -n "$CUE_SHEET" ]; then
+    show_tracks "$@" \
+	| make_cue_sheet
+else
+    show_tracks "$@" \
+	| process_tracks \
+	| ${NUMBERING[@]:-cat -n}
+fi
