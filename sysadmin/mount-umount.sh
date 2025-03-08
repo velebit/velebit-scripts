b6a5293ec7e383394950a477c0803b4360fab35b
bert <velebit@users.noreply.github.com>
2025-03-08 11:09:47 -0500

    user/{,u}mount-raid-*: Add SMB/SSHFS scripts

--- sysadmin/mount-umount.sh
+++ sysadmin/mount-umount.sh
@@ -1,5 +1,7 @@
 #!/sourceable/code/for/bash
 
+##### mounting local partitions #####
+
 find_partition () {
     local partlabel="$1"; shift
     local seen=()
@@ -183,6 +185,157 @@ do_umount () {
         if ! eject_partition "$part"; then
             return 4
         fi
+    fi
+    return 0
+}
+
+##### mounting remote filesystems #####
+
+is_local_address () {
+    local host_addr="$1"; shift
+    if [ -n "$(ip addr show to "$host_addr/32")" ]; then
+	return 0  # address matches a local interface
+    else
+	return 1  # address is remote
+    fi
+}
+
+create_mountpoint () {
+    local dir="$1"; shift
+    local parent="$1"; shift
+    if [[ ! -d "$dir" ]]; then
+	if [[ -n "$parent" ]] && [[ ! -d "$parent" ]]; then
+            echo "Warning: $parent does not exist." >&2
+	fi
+	if ! mkdir -p "$dir"; then
+            echo "Error: could not create $dir!" >&2
+	    return 1
+	fi
+    fi
+    return 0
+}
+
+remove_mountpoint () {
+    local dir="$1"; shift
+    if ! rmdir "$dir"; then
+        echo "Warning: could not remove $dir!" >&2
+	return 1
+    fi
+    return 0
+}
+
+construct_remote_path () {
+    host_name="$1"; shift
+    share="$1"; shift
+    case "$host_name:$share" in
+	aunt-louisa:*)  echo "/common/$share/export" ;;
+    esac
+}
+
+do_mount_smb () {
+    local host_name="$1"; shift
+    local host_addr="$1"; shift
+    local share="$1"; shift
+
+    local user="$(id -un)"
+    #local type=cifs
+    local type=smb3
+    local parent=/media/"$user"/"$type"
+    local dir="$parent"/"$host_name"/"$share"
+
+    if is_local_address "$host_addr"; then
+	echo "Error: requested mounting on the server." >&2
+	return 1
+    fi
+    if ! create_mountpoint "$dir" "$parent"; then
+	return 1  # message already shown
+    fi
+
+    sudo mount -t "$type" -o username="$user",rw \
+	 //"$host_addr"/"$share" "$dir"
+}
+
+
+do_umount_smb () {
+    local host_name="$1"; shift
+    local host_addr="$1"; shift
+    local share="$1"; shift
+
+    local user="$(id -un)"
+    #local type=cifs
+    local type=smb3
+    local parent=/media/"$user"/"$type"
+    local dir="$parent"/"$host_name"/"$share"
+
+    if [[ ! -d "$dir" ]]; then
+	echo "Warning: $dir does not exist, skipped." >&2
+	return 0
+    fi
+    if ! sudo umount "$dir"; then
+	echo "Warning: could not unmount $dir." >&2
+	return 1
+    fi
+    if ! remove_mountpoint "$dir"; then
+	return 1  # message already shown
+    fi
+    return 0
+}
+
+
+do_mount_sshfs () {
+    local host_name="$1"; shift
+    local host_addr="$1"; shift
+    local share="$1"; shift
+    local rpath="$1"; shift  # optional
+
+    if [[ -z "$rpath" ]]; then
+	rpath="$(construct_remote_path "$host_name" "$share")"
+	if [[ -z "$rpath" ]]; then return 2; fi
+    fi
+
+    local user="$(id -un)"
+    local type=ssh
+    local parent=/media/"$user"/"$type"
+    local dir="$parent"/"$host_name"/"$share"
+
+    if is_local_address "$host_addr"; then
+	echo "Error: requested mounting on the server." >&2
+	return 1
+    fi
+    if ! create_mountpoint "$dir" "$parent"; then
+	return 1  # message already shown
+    fi
+
+    # assume ssh-agent may be holding credentials for Git
+    local ssh_options=( -o PubkeyAuthentication=no
+			-o PasswordAuthentication=yes )
+    sshfs "$user"@"$host_addr":"$rpath" "$dir" "${ssh_options[@]}"
+}
+
+
+do_umount_sshfs () {
+    local host_name="$1"; shift
+    local host_addr="$1"; shift
+    local share="$1"; shift
+
+    local user="$(id -un)"
+    local type=ssh
+    local parent=/media/"$user"/"$type"
+    local dir="$parent"/"$host_name"/"$share"
+
+    # umount_cmd=( fusermount3 -u "$dir" )   # Linux only
+    umount_cmd=( umount "$dir" )             # recent Linux, OS X, *BSD...
+
+    if [[ ! -d "$dir" ]]; then
+	echo "Warning: $dir does not exist, skipped." >&2
+	return 0
+    fi
+    if ! "${umount_cmd[@]}"; then
+	echo "Warning: could not unmount $dir." >&2
+	return 1
+    fi
+    if ! remove_mountpoint "$dir"; then
+	return 1  # message already shown
     fi
     return 0
 }
