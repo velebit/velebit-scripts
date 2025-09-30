81ae6852592d945bc647168152d74addd67501ad
bert <velebit@users.noreply.github.com>
2025-09-30 08:28:32 -0400

    [NCFO] Update audition download script for 2026
    The majority of changes are shellcheck related.

--- net-slurp/ncfo/audition/download.sh
+++ net-slurp/ncfo/audition/download.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
-page=2024_Rain_Dance_Auditions
-file="`basename "$page"`"
+page=2026_Weedpatch_Auditions
+file="$(basename "${page}")"
 type=files
 dir=files
-log=download-"$type".log
-if [ -f "$dir"/index.html ]; then
-    rm -f "$dir/$file"; mv "$dir"/index.html "$dir/$file"; fi
-cp -p "$dir/$file" "$dir/$file".orig
-wget --load-cookies cookies.txt \
-    -nd -P "$dir" -N --restrict-file-names=windows \
-    -r -l 1 -A .mp3,.MP3,.pdf,.PDF,"$file" \
+log=download-"${type}".log
+if [ -f "${dir}"/index.html ]; then
+    rm -f "${dir}/${file}"; mv "${dir}"/index.html "${dir}/${file}"; fi
+cp -p "${dir}/${file}" "${dir}/${file}".orig
+wget \
+    -nd -P "${dir}" -N --restrict-file-names=windows \
+    -r -l 1 -A .mp3,.MP3,.pdf,.PDF,"${file}" \
     --reject-regex '^http://www.familyopera.org/drupal/$' \
     --progress=bar:force \
-    http://www.familyopera.org/drupal/"$page" \
-  2>&1 | tee "$log"
-if [ -f "$dir/$file" ]; then
-    rm -f "$dir"/index.html "$dir/$file".orig
-    mv "$dir/$file" "$dir"/index.html
-    #./clean-up.pl -i "$file" -i index.html "$log"
-elif [ -f "$dir/$file".orig ]; then
-    rm -f "$dir"/index.html "$dir/$file"
-    mv "$dir/$file".orig "$dir"/index.html
+    http://www.familyopera.org/drupal/"${page}" \
+  2>&1 | tee "${log}"
+if [ -f "${dir}/${file}" ]; then
+    rm -f "${dir}"/index.html "${dir}/${file}".orig
+    mv "${dir}/${file}" "${dir}"/index.html
+    #./clean-up.pl -i "${file}" -i index.html "${log}"
+elif [ -f "${dir}/${file}".orig ]; then
+    rm -f "${dir}"/index.html "${dir}/${file}"
+    mv "${dir}/${file}".orig "${dir}"/index.html
     echo "(index not downloaded)"
 else
     echo "(index not downloaded, original missing)"
