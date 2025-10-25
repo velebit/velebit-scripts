44af0c353b594d29187d9563ef64330408c4476e
bert <velebit@users.noreply.github.com>
2025-10-25 07:38:47 -0400

    lo-fi: Complain if comparing with the same dir

--- music/lo-fi/compare-list-of-files.sh
+++ music/lo-fi/compare-list-of-files.sh
@@ -7,8 +7,27 @@ canonical_file_list () {
          -o -print | sed -e 's,\.\(m4a\|flac\|mp3\)$,.<audio>,' | sort
 }
 
-canonical_file_list > /tmp/fl_ours.txt
-(cd "$(dirname "$0")"; canonical_file_list > /tmp/fl_lofi.txt)
+dir1="."
+dir2="$(dirname "$0")"
+path1="$(realpath "${dir1}")"
+path2="$(realpath "${dir2}")"
+if [[ -z "${dir1}" ]]; then
+    echo "Error: bad path '${dir1}'" >&2; exit 1
+elif [[ -z "${dir2}" ]]; then
+    echo "Error: bad path '${dir2}'" >&2; exit 1
+elif [[ "${dir1}" == "${dir2}" ]]; then
+    echo "Error: running from ${dir2}, should be different from ${dir1}" >&2
+    exit 1
+elif [[ -z "${path1}" ]]; then
+    echo "Error: realpath failed on '${dir1}'" >&2; exit 1
+elif [[ -z "${path2}" ]]; then
+    echo "Error: realpath failed on '${dir2}'" >&2; exit 1
+elif [[ "${path1}" == "${path2}" ]]; then
+    echo "Error: ${dir1} and ${dir2} both refer to ${path1}" >&2; exit 1
+fi
+
+(cd "${dir1}"; canonical_file_list > /tmp/fl_ours.txt)
+(cd "${dir2}"; canonical_file_list > /tmp/fl_lofi.txt)
 if [[ -n "$DISPLAY" ]]; then
     meld /tmp/fl_ours.txt /tmp/fl_lofi.txt
 else
